4f000d2a64f1e20e0b70d0671aa56ae1e853d67a
Tom DeVito <tom@teltech.co>
2015-04-06 14:22:38 -0400

    Default multi publisher/subscriber uses basic rabbitmq environment variables if multiple rabbitmq environment variables are not present.

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"github.com/microplatform-io/platform"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"time"
 
 	"github.com/codegangsta/negroni"
@@ -18,6 +20,9 @@ var (
 	rabbitPass = os.Getenv("RABBITMQ_PASS")
 	rabbitAddr = os.Getenv("RABBITMQ_PORT_5672_TCP_ADDR")
 	rabbitPort = os.Getenv("RABBITMQ_PORT_5672_TCP_PORT")
+
+	rabbitRegex            = regexp.MustCompile("RABBITMQ_[0-9]_PORT_5672_TCP_(ADDR|PORT)")
+	amqpConnectionManagers []*platform.AmqpConnectionManager
 )
 
 type Request struct {
@@ -30,17 +35,9 @@ type Request struct {
 func main() {
 	hostname, _ := os.Hostname()
 
-	connMgr := platform.NewAmqpConnectionManager(rabbitUser, rabbitPass, rabbitAddr+":"+rabbitPort, "")
+	publisher := getDefaultMultiPublisher()
 
-	publisher, err := platform.NewAmqpPublisher(connMgr)
-	if err != nil {
-		log.Fatalf("> failed to create publisher: %s", err)
-	}
-
-	subscriber, err := platform.NewAmqpSubscriber(connMgr, "router_"+hostname)
-	if err != nil {
-		log.Fatalf("> failed to create subscriber: %s", err)
-	}
+	subscriber := getDefaultMultiSubscriber("router_" + hostname)
 
 	standardRouter := platform.NewStandardRouter(publisher, subscriber)
 
@@ -128,4 +125,73 @@ func main() {
 	}))
 	n.UseHandler(mux)
 	n.Run(":80")
+}
+
+func getDefaultMultiPublisher() platform.Publisher {
+
+	publishers := []platform.Publisher{}
+
+	connMgrs := getAmqpConnectionManagers()
+	for _, connMgr := range connMgrs {
+
+		publisher, err := platform.NewAmqpPublisher(connMgr)
+		if err != nil {
+			log.Printf("Could not create publisher. %s", err)
+			continue
+		}
+		publishers = append(publishers, publisher)
+	}
+
+	if len(publishers) == 0 {
+
+		log.Fatalf("Failed to create a single publisher: %s\n")
+	}
+
+	return platform.NewMultiPublisher(publishers...)
+
+}
+
+func getDefaultMultiSubscriber(queue string) platform.Subscriber {
+	subscribers := []platform.Subscriber{}
+	connMgrs := getAmqpConnectionManagers()
+	for _, connMgr := range connMgrs {
+
+		subscriber, err := platform.NewAmqpSubscriber(connMgr, queue)
+		if err != nil {
+			log.Printf("Could not create subscriber. %s", err)
+			continue
+		}
+		subscribers = append(subscribers, subscriber)
+	}
+
+	if len(subscribers) == 0 {
+		log.Fatalf("Failed to create a single subscriber: %s\n")
+	}
+
+	return platform.NewMultiSubscriber(subscribers...)
+}
+
+func getAmqpConnectionManagers() []*platform.AmqpConnectionManager {
+	if amqpConnectionManagers != nil {
+		return amqpConnectionManagers
+	}
+
+	amqpConnectionManagers := []*platform.AmqpConnectionManager{}
+
+	count := 0
+	for _, v := range os.Environ() {
+		if rabbitRegex.MatchString(v) {
+			count++
+		}
+	}
+
+	if count == 0 { // No match for multiple rabbitmq servers, try and use single rabbitmq environment variables
+		amqpConnectionManagers = append(amqpConnectionManagers, platform.NewAmqpConnectionManager(rabbitUser, rabbitPass, rabbitAddr+":"+rabbitPort, ""))
+	} else if count%2 == 0 { // looking for a piar or rabbitmq addr and port
+		for i := 0; i < count/2; i++ {
+			amqpConnectionManagers = append(amqpConnectionManagers, platform.NewAmqpConnectionManager(rabbitUser, rabbitPass, fmt.Sprintf("%s:%s", os.Getenv(fmt.Sprintf("RABBITMQ_%d_PORT_5672_TCP_ADDR", i+1)), os.Getenv(fmt.Sprint("RABBITMQ_%d_PORT_5672_TCP_PORT", i+1))), ""))
+		}
+	}
+
+	return amqpConnectionManagers
 }
