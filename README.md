e40d389b5723bb77db250b4311b52db12636bf17
Bryan Moyles <bryan@teltech.co>
2016-01-21 10:17:01 -0500

    Using platform shortcuts for publishers and subscribers

--- README.md
+++
@@ -1,1 +0,0 @@
-# platform-router-websocket
--- main.go
+++ main.go
@@ -2,80 +2,18 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/microplatform-io/platform"
-)
-
-const (
-	SSL_CERT_FILE = "/tmp/ssl_cert"
-	SSL_KEY_FILE  = "/tmp/ssl_key"
 )
 
 var (
 	rabbitmqEndpoints = strings.Split(os.Getenv("RABBITMQ_ENDPOINTS"), ",")
 	routerPort        = platform.Getenv("PORT", "80")
 )
-
-type MultiPublisher struct {
-	publishers []platform.Publisher
-
-	offset int
-}
-
-func (p *MultiPublisher) Publish(topic string, body []byte) error {
-	defer p.incrementOffset()
-
-	return p.publishers[p.offset].Publish(topic, body)
-}
-
-func (p *MultiPublisher) incrementOffset() {
-	p.offset = (p.offset + 1) % len(p.publishers)
-}
-
-func NewMultiPublisher(publishers []platform.Publisher) *MultiPublisher {
-	return &MultiPublisher{
-		publishers: publishers,
-	}
-}
-
-type MultiSubscriber struct {
-	subscribers []platform.Subscriber
-}
-
-func (s *MultiSubscriber) Run() error {
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
-
-	for i := range s.subscribers {
-		go func(i int) {
-			s.subscribers[i].Run()
-
-			wg.Done()
-		}(i)
-	}
-
-	wg.Wait()
-
-	return nil
-}
-
-func (s *MultiSubscriber) Subscribe(topic string, handler platform.ConsumerHandler) {
-	for i := range s.subscribers {
-		s.subscribers[i].Subscribe(topic, handler)
-	}
-}
-
-func NewMultiSubscriber(subscribers []platform.Subscriber) *MultiSubscriber {
-	return &MultiSubscriber{
-		subscribers: subscribers,
-	}
-}
 
 type ServerConfig struct {
 	Protocol string `json:"protocol"`
@@ -95,26 +33,20 @@ func main() {
 
 	routerUri := "router-" + serverIpAddr + "-" + hostname
 
-	publishers := []platform.Publisher{}
-	subscribers := []platform.Subscriber{}
+	connectionManagers := platform.NewAmqpConnectionManagersWithEndpoints(rabbitmqEndpoints)
 
-	for i := range rabbitmqEndpoints {
-		connectionManager := platform.NewAmqpConnectionManagerWithEndpoint(rabbitmqEndpoints[i])
-
-		publishers = append(publishers, getDefaultPublisher(connectionManager))
-		subscribers = append(subscribers, getDefaultSubscriber(connectionManager, routerUri))
+	publisher, err := platform.NewAmqpMultiPublisher(connectionManagers)
+	if err != nil {
+		log.Fatalf("> failed to create multi publisher: %s", err)
 	}
 
-	router := platform.NewStandardRouter(NewMultiPublisher(publishers), NewMultiSubscriber(subscribers))
+	subscriber, err := platform.NewAmqpMultiSubscriber(connectionManagers, routerUri)
+	if err != nil {
+		log.Fatalf("> failed to create multi subscriber: %s", err)
+	}
+
+	router := platform.NewStandardRouter(publisher, subscriber)
 	router.SetHeartbeatTimeout(7 * time.Second)
-
-	if err := ioutil.WriteFile(SSL_CERT_FILE, []byte(strings.Replace(os.Getenv("SSL_CERT"), "\\n", "\n", -1)), 0755); err != nil {
-		log.Fatalf("> failed to write SSL cert file: %s", err)
-	}
-
-	if err := ioutil.WriteFile(SSL_KEY_FILE, []byte(strings.Replace(os.Getenv("SSL_KEY"), "\\n", "\n", -1)), 0755); err != nil {
-		log.Fatalf("> failed to write SSL cert file: %s", err)
-	}
 
 	socketioServer, err := CreateSocketioServer(serverIpAddr, router)
 	if err != nil {
@@ -128,24 +60,6 @@ func main() {
 	}, router)
 	mux.Handle("/socket.io/", socketioServer)
 	ListenForHttpServer(routerUri, mux)
-}
-
-func getDefaultPublisher(connectionManager *platform.AmqpConnectionManager) platform.Publisher {
-	publisher, err := platform.NewAmqpPublisher(connectionManager)
-	if err != nil {
-		log.Fatalf("Could not create publisher. %s", err)
-	}
-
-	return publisher
-}
-
-func getDefaultSubscriber(connectionManager *platform.AmqpConnectionManager, queue string) platform.Subscriber {
-	subscriber, err := platform.NewAmqpSubscriber(connectionManager, queue)
-	if err != nil {
-		log.Fatalf("Could not create subscriber. %s", err)
-	}
-
-	return subscriber
 }
 
 func formatHostAddress(ip string) string {
