751067d02b9b033061a387dadf37ba8c266bee47
Bryan Moyles <bryan@teltech.co>
2016-06-14 16:18:50 -0400

    Specifying the cipher suite, let's see if it improves things

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -117,7 +118,24 @@ func main() {
 			wrappedMux := &AccessControlMiddleware{&LoggingMiddleware{mux}}
 
 			logger.Println("Serving HTTPS: " + PORT_HTTPS)
-			err = http.ListenAndServeTLS(":"+PORT_HTTPS, certFile.Name(), keyFile.Name(), wrappedMux)
+			cfg := &tls.Config{
+				MinVersion:               tls.VersionTLS12,
+				CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+				PreferServerCipherSuites: true,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				},
+			}
+			srv := &http.Server{
+				Addr:         ":" + PORT_HTTPS,
+				Handler:      wrappedMux,
+				TLSConfig:    cfg,
+				TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+			}
+			err = srv.ListenAndServeTLS(certFile.Name(), keyFile.Name())
 			logger.Fatalf("HTTPS server has died: %s", err)
 		}()
 	}
