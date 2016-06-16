c08a388e4466820be65bc306284dbd07c0b0283c
Bryan Moyles <bryan@teltech.co>
2016-06-16 17:54:50 -0400

    Restoring 256 bit encrpytion and asking clients to connect to http

--- main.go
+++ main.go
@@ -112,9 +112,9 @@ func main() {
 			ioutil.WriteFile(keyFile.Name(), []byte(strings.Replace(SSL_KEY, "\\n", "\n", -1)), os.ModeTemporary)
 
 			mux := CreateServeMux(&ServerConfig{
-				Protocol: "https",
+				Protocol: "http",
 				Host:     formatHostAddress(externalIP),
-				Port:     PORT_HTTPS,
+				Port:     PORT_HTTP,
 				ClientArgs: map[string]interface{}{
 					"upgrade": CLIENT_UPGRADE == "1" || strings.ToLower(CLIENT_UPGRADE) == "true",
 				},
@@ -129,10 +129,10 @@ func main() {
 				CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 				PreferServerCipherSuites: true,
 				CipherSuites: []uint16{
-					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-					tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-					tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-					tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 				},
 			}
 			srv := &http.Server{
